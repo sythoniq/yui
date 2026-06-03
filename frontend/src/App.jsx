479af46eb8c5a2b38dcd41a35145842a5ffaec10
sythe <188770367+sythoniq@users.noreply.github.com>
2026-06-03 21:15:21 +0300

    feat: trying to start working on the frontend, kinda sick atm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 export default function App() {
   return (
-	<>
-	<header className="page-heading"></header>
-	<main className="main-container"></main>
-	</>
+    <>
+      <header className="page-heading">
+        <h2>結Yui</h2>
+      </header>
+      <main className="main-container">
+        
+      </main>
+    </>
   )
 }
---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,13 @@
+export default function User(props) {
+  return (
+    <>
+      <div className="user-card">
+        <img src="#" alt="generic user profile img" />
+        <div className="user-details">
+          <h2>UserName</h2>
+          <p>Last Message Preview if none then have a small quote</p>
+        </div>
+      </div>
+    </>
+  )
+}
