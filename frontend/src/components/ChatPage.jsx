817b6e4bf09e23c9ecb4a9909d627cf55e13d359
sythe <188770367+sythoniq@users.noreply.github.com>
2026-06-08 21:07:31 +0300

    this project is so dead D:

---
+++ frontend/src/components/ChatPage.jsx
@@ -0,0 +1,7 @@
+export default function ChatPage(props) {
+				return (
+								<p>
+												The actual chat page.
+								</p>
+				)
+}
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,8 +1,13 @@
 import App from '../App.jsx'
+import ChatPage from '../components/ChatPage.jsx';
 
 const routes = [
   {
     path: "/chat/:userId",
-    
+    element: <ChatPage />
+  },
+  {
+    path: "/",
+    index: true, element: <App />
   }
 ]
