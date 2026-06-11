b4c500eb9d41051c0ae88cf691d68e697b310ac2
sythe <188770367+sythoniq@users.noreply.github.com>
2026-06-11 19:22:06 +0300

    fix(removal): Removed the search option implementation for now

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,14 +2,11 @@ import { Link } from 'react-router'
 import { useState } from 'react'
 
 export default function Header(props) {
-				const [searchData, setSearchData] = useState('')
-
 				return (
 								<header className="page-heading">
 												<Link to="/"><h2>結Yui</h2></Link>
 												<nav className="heading-nav">
-																{/* Have a search button here to search for a user */}
-																<input type="text" placeholder="Search" onChange={(e) => setSearchData(e.target.value)}/>
+																{/* Have a search button here to search for a user as a extra feature*/}
 																<Link to="/about">About</Link>
 																<Link to="/profile">UserProf</Link>
 												</nav>
