11de215dd8648b31b0e2db2c9665eb05b0c16e0d
sythe <188770367+sythoniq@users.noreply.github.com>
2026-06-17 15:41:33 +0300

    feat: added the send chat route and get chat controllers, still very flawed overall

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,9 +1,46 @@
+const prisma = require("../configs/prisma.js")
+
 async function getChat(req, res, next) {
-				// TODO: Get user chat with the specific user specified in the userId param.
+				try {
+								const currentUser = req.user;
+								const messages = await prisma.user.findUnique({
+												where: {
+																userId: currentUser.userId
+												},
+												include: {
+																sentMessages: true,
+																receivedMessages: true
+												}
+								})
+
+								return res.json({success: true, messages: {
+												sent: messages.sentMessages,
+												received: messages.receivedMessages
+								}});
+				} catch(e) {
+								console.log(e);
+								next(e);
+				}
 }
 
 async function sendMessage(req, res, next) {
-				//TODO: Send message to user with the userId param as the receiver.
+				try {
+								const sender = req.user.userId;
+								const receiver = req.params.userId;
+
+								const message = await prisma.message.create({
+												data: {
+																messageContent: req.body.messageBody,
+																receiverId: Number(receiver),
+																senderId: Number(sender)
+												}
+								})
+
+								return res.json({success: true, message});
+				} catch (e) {
+								console.log(e);
+								next(e)
+				}
 }
 
 module.exports = {
--- backend/src/routes/chat.js
+++ backend/src/routes/chat.js
@@ -1,10 +1,12 @@
 const { Router } = require("express")
 const chat = Router();
 
+const auth = require("../configs/helpers.js").authUser;
+
 const controller = require("../controllers/chatController.js")
 
-chat.get("/:userId", controller.getChat);
+chat.get("/:userId", auth, controller.getChat);
 
-chat.post("/:userId/send", controller.sendMessage);
+chat.post("/:userId/send", auth, controller.sendMessage);
 
 module.exports = chat;
