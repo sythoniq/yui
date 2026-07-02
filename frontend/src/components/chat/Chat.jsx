f464bd0038f65f5524832dfe52b9383be84a46bc
sythe <188770367+sythoniq@users.noreply.github.com>
2026-07-02 19:10:13 +0300

    feat: working on rendering the chat messages, problem is messages will be in blocks.. need to figure out how to make it a flowing continuous one from one message to another according to time sent/received

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,40 +1,21 @@
 import "./Chat.css"
 
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
+import { useParams, useLoaderData } from 'react-router'
 
 export default function Chat() {
-				const API = import.meta.env.VITE_BASE_API
 				const { userId } = useParams()
+				const messages = useLoaderData()
 
-				const [receivedMessages, setReceivedMessages] = useState()
-				const [sentMessages, setSentMessages] = useState()
-
-				useEffect(() => {
-								async function getMessages() {
-												const res = await fetch(`${API}/chat/${userId}`, {
-																headers: {
-																				"Content-Type": "application/json",
-																				"Authorization": localStorage.getItem("jwt-token")
-																}
-												});
-												const data = await res.json()
-												if (!data.success) {
-																console.error("Error fetching messages")
-												}
-
-												const messages = data.messages;
-												setSentMessages(messages.sentMessages)
-												setReceivedMessages(messages.receivedMessages);
-								}
-
-								return () => {
-												getMessages()
-								}
-				}, [])
+				if (messages == undefined) {
+								return (
+												<p>Loading...</p>
+								)
+				}
 
 				return (
 								<main className="chat-body">
+
 								</main>
 				)
 }
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -4,6 +4,26 @@ import Register from '../components/register/Register.jsx'
 import Chat from '../components/chat/Chat.jsx'
 import UserPage from '../components/users/UserPage.jsx'
 
+const API = import.meta.env.VITE_BASE_API
+
+async function getMessages({params}) {
+				const res = await fetch(`${API}/chat/${params.userId}`, {
+								headers: {
+												"Content-Type": "application/json",
+												"Authorization": localStorage.getItem("jwt-token")
+								}
+				});
+				const data = await res.json()
+				if (!data.success) {
+								console.error("Error fetching messages")
+				}
+
+				const messages = data.messages;
+
+				return messages;
+}
+
+
 const routes = [
 				{
 								path: "/",
@@ -11,7 +31,8 @@ const routes = [
 								children: [
 												{
 																path: "/chat/:userId",
-																element: <Chat />
+																element: <Chat />,
+																loader: getMessages
 												}
 								]
 				},
