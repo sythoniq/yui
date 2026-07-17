fc321f4527dfc54d11f8c5febca315fb31be1b8b
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-07-17 19:31:20 +0300

    fix: made some fixes to handle some edge cases within the entire backend and added comments/todos for issues that need prompt handling

--- backend/src/controllers/chatCont.js
+++ backend/src/controllers/chatCont.js
@@ -1,92 +1,109 @@
 const prisma = require("../libs/prisma.js")
 
 async function getChat(req, res) {
-				try {
-								// Auth check
-								if (!req.user) {
-												return res.status(401).json({message: "Unathorized"})
-								}
-								const currentUserId = Number(req.user.userid);
-								const recipientId = Number(req.params.userId);
+	try {
+		// Auth check
+		if (!req.user) {
+			return res.status(401).json({success: false, message: "Unauthorized to chat"})
+		}
 
-								const messages = await prisma.message.findMany({
-												where: {
-																OR: [
-																				{
-																								sender_id: currentUserId, receiver_id: recipientId
-																				},
-																				{
-																								sender_id: recipientId, receiver_id: currentUserId
-																				}
-																]
-												},
-												orderBy: {
-																message_date: "asc"
-												},
-												include: {
-																sender: {
-																				omit: {
-																								user_hash: true
-																				}
-																},
-																receiver: {
-																				omit: {
-																								user_hash: true
-																				}	
-																}
-												},
-								})
+		const currentUserId = Number(req.user.userid);
+		const recipientId = Number(req.params.userId);
 
-								const recipient = await prisma.user.findUnique({
-												where: {
-																user_id: recipientId
-												},
-												omit: {
-																user_hash: true
-												}
-								})
+		const recipient = await prisma.user.findUnique({
+			where: {
+				user_id: recipientId
+			},
+			omit: {
+				user_hash: true
+			}
+		})
 
-								return res.json({
-												success: true,
-												messages,
-												recipient
-								})
-				} catch(e) {
-								console.error(e)
-								return res.status(500).json({success: false, message: "Unexpected error occurred"})
+		if (!recipient) {
+			return res.status(404).json({success: false, message: "Recipient user not found"})
+		}
+
+		const messages = await prisma.message.findMany({
+			where: {
+				OR: [
+					{
+						sender_id: currentUserId, receiver_id: recipientId
+					},
+					{
+						sender_id: recipientId, receiver_id: currentUserId
+					}
+				]
+			},
+			orderBy: {
+				message_date: "asc"
+			},
+			include: {
+				sender: {
+					omit: {
+						user_hash: true
+					}
+				},
+				receiver: {
+					omit: {
+						user_hash: true
+					}	
 				}
+			},
+		})
+
+		return res.json({
+			success: true,
+			messages,
+			recipient
+		})
+	} catch(e) {
+		return res.status(500).json({success: false, message: "Unexpected error occurred"})
+	}
 }
 
 async function sendChat(req, res) {
-				try {
-								if (!req.user) {
-												return res.status(401).json({message: "Unauthorized"})
-								}
+	try {
+		if (!req.user) {
+			return res.status(401).json({ success: false, message: "Unauthorized to send message"})
+		}
 
-								const senderId = Number(req.user.userid);
-								const receiverId = Number(req.params.userId);
+		const senderId = Number(req.user.userid);
+		const receiverId = Number(req.params.userId);
 
-								const messageBody = req.body.message
+		const receiver = await prisma.user.findUnique({
+			where: {
+				user_id: receiverId
+			}
+		})
 
-								const message = await prisma.message.create({
-												data: {
-																message_content: messageBody,
-																sender_id: senderId,
-																receiver_id: receiverId
-												}
-								})
+		if (!receiver) {
+			return res.status(404).json({success: false, message: "Recipient user not found"})
+		}
 
-								return res.json({
-												success: true,
-												message
-								})
-				}catch(e) {
-								console.log(e)
-								return res.json({success: false, message: "Database error sending message"})
-				}
+		const messageBody = req.body.message
+
+		if (!messageBody) {
+			return res.status(500).json({success: false, message: "Message body not found"})
+		}
+
+		const message = await prisma.message.create({
+			data: {
+				message_content: messageBody,
+				sender_id: senderId,
+				receiver_id: receiverId
+			}
+		})
+
+		return res.json({
+			success: true,
+			message
+		})
+	}catch(e) {
+		return res.status(500).json({success: false, message: "Unexpected error occured"})
+	}
 }
 
 module.exports = {
-				getChat,
-				sendChat
+	getChat,
+	sendChat
 }
--- backend/src/controllers/rootCont.js
+++ backend/src/controllers/rootCont.js
@@ -7,209 +7,222 @@ const bcrypt = require("bcryptjs")
 
 const prisma = require("../libs/prisma.js")
 
+// ERROR: Need to scrap this and properly implement a owner based upload system with supabase... literally a useless function atm
 async function uploadProfileImage(file, user) {
-				// Function to upload the users profile image to supabase just so that I can clean up abit
-				const { data, error } = await supabase.storage.from('yui').upload(`/${file.originalname}`, file.buffer, {
-								upsert: true,
-								contentType: file.mimetype
-				})
+	// Function to upload the users profile image to supabase just so that I can clean up abit
+	const { data, error } = await supabase.storage.from('yui').upload(`/${file.originalname}`, file.buffer, {
+		upsert: true,
+		contentType: file.mimetype
+	})
 
-				if (error) {
-								return error;
+	if (error) {
+		return error;
+	}
+
+	const response = supabase.storage.from("yui").getPublicUrl(`/${file.originalname}`);
+
+	if (user.profile_image.length <= 0) {
+		const profile = await prisma.user.update({
+			where: {
+				user_id: user.user_id
+			},
+			data: {
+				profile_image: {
+					create: {
+						image_url: response.data.publicUrl
+					}
 				}
+			}
+		})
 
-				const response = supabase.storage.from("yui").getPublicUrl(`/${file.originalname}`);
+		return profile;
+	}
 
-				if (user.profile_image.length <= 0) {
-								const profile = await prisma.user.update({
-												where: {
-																user_id: user.user_id
-												},
-												data: {
-																profile_image: {
-																				create: {
-																								image_url: response.data.publicUrl
-																				}
-																}
-												}
-								})
+	const imgId = user.profile_image[0].image_id;
+	const profile = await prisma.profile.update({
+		where: {
+			image_id: imgId
+		},
+		data: {
+			image_url: response.data.publicUrl
+		}
+	})
 
-								return profile;
-				}
-
-				const imgId = user.profile_image[0].image_id;
-				const profile = await prisma.profile.update({
-								where: {
-												image_id: imgId
-								},
-								data: {
-												image_url: response.data.publicUrl
-								}
-				})
-
-				return profile
+	return profile
 }
 
-async function registerUser(req, res, next) {
-				try {
-								const { username, password } = req.body 
+async function registerUser(req, res) {
+	try {
+		const { username, password } = req.body 
 
-								const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-								const user = await prisma.user.create({
-												data: {
-																user_name: username,
-																user_hash: hashedPassword
-												}
-								})
-								return res.json({
-												success: true,
-												user: {
-																userId: user.user_id,
-																userName: user.user_name
-												}
-								})
-				} catch(e) {
-								return res.status(500).json({success: false, message: "Error creating user"});
-				}
+		const user = await prisma.user.create({
+			data: {
+				user_name: username,
+				user_hash: hashedPassword
+			}
+		})
+		return res.json({
+			success: true,
+			user: {
+				userId: user.user_id,
+				userName: user.user_name
+			}
+		})
+	} catch(e) {
+		return res.status(500).json({success: false, message: "Failed to create user", error: e})
+	}
 }
 
 async function loginUser(req, res) {
-				try {
-								const { username, password } = req.body;
+	try {
+		const { username, password } = req.body;
 
-								const user = await prisma.user.findUnique({
-												where: {
-																user_name: username
-												}
-								})
+		const user = await prisma.user.findUnique({
+			where: {
+				user_name: username
+			}
+		})
 
-								if (!user) {
-												return res.json(404).json({success: false, message: "User not found"})
-								}
-								
-								const result = await bcrypt.compare(password, user.user_hash)
+		if (!user) {
+			return res.json(404).json({success: false, message: "User not found"})
+		}
 
-								if (!result) {
-												return res.json(401).json({success: false, message: "Incorrect password..."})
-								}
+		await bcrypt.compare(password, user_hash, (err, result) => {
+			if (err) {
+				throw new Error("Error occured during password validation.")
+			}
+			if (!result) {
+				throw new Error("Invalid password")
+			}
 
-								const token = jwt.sign({userid: user.user_id, iat: Date.now()}, process.env.JWT_SECRET, {expiresIn: '7d'});
-
-								return res.json({
-												success: true,
-												token
-								})
-				} catch(e) {
-								console.error(e);
-								return res.status(500).json({success: false, message: "Failed to login the user"})
-				} 
+			const token = jwt.sign({userid: user.user_id, iat: Date.now()}, process.env.JWT_SECRET, {expiresIn: '7d'});
+			return res.status(200).json({ success: true, token })
+		})
+	} catch(e) {
+		return res.status(500).json({success: false, message: "Failed to login the user", error: e})
+	} 
 }
 
 async function getUsers(req, res) {
-				try {
-								let users = await prisma.user.findMany({
-												omit: {
-																user_hash: true
-												}
-								});
+	try {
+		let users = await prisma.user.findMany({
+			omit: {
+				user_hash: true
+			}
+		});
 
-								if (req.user == null) {
-												return res.json({sucess: true, users})
-								}
+		if (req.user == null) {
+			return res.json({sucess: true, users})
+		}
 
-								const userId = req.user.userid;
-								users = users.filter((usr) => usr.user_id != userId);
+		const userId = req.user.userid;
+		users = users.filter((usr) => usr.user_id != userId);
 
-								return res.json({sucess: true, users});
-				} catch (e) {
-								console.error(e)
-				}
+		return res.json({sucess: true, users});
+	} catch (e) {
+		res.status(501).json({success: false, error: "Failed to fetch users!"})
+	}
 }
 
 async function authUser(req, res) {
-				const authHeader = req.headers["authorization"]
-				if (!authHeader) {
-								return res.json({success: false, message: "Failed to auth"})
-				}
-				const token = authHeader.split(" ")[1]
+	const token = req.headers["authorization"].split(" ")[1]
 
-				const result = jwt.verify(token, process.env.JWT_SECRET);
-				if (result) {
-								const user = await prisma.user.findUnique({
-												where: {
-																user_id: Number(result.userid)
-												},
-												omit: {
-																user_hash: true
-												},
-												include: {
-																profile_image: true
-												}
-								})
+	if (!token) {
+		return res.json({success: false, message: "Failed to verify user!"})
+	}
 
-								return res.json({success: true, message: "Authed", user})
+	jwt.verify(token, process.env.JWT_SECRET, async (error, result) => {
+		if (error) {
+			return res.status(401).json({success: false, error: "Failed to verify user!", err: error})
+		}
+		if (result) {
+			const user = await prisma.user.findUnique({
+				where: {
+					user_id: Number(result.userid)
+				},
+				omit: {
+					user_hash: true
+				},
+				include: {
+					profile_image: true
 				}
-				return res.json({success: false, message: "Unauthed"})
+			})
+			return res.json({success: true, message: "User verified", user})
+		}});
 }
 
 async function getUserProfile(req, res) {
-				const user = await prisma.user.findUnique({
-								where: {
-												user_id: Number(req.params.userId),
-								},
-								omit: {
-												user_hash: true
-								},
-								include: {
-												profile_image: {
-																select: {
-																				image_url: true
-																}
-												}
-								}
-				})
-
-				if (!user) {
-								return res.status(404).json({success: false, message: "User not found"})
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				user_id: Number(req.params.userId),
+			},
+			omit: {
+				user_hash: true
+			},
+			include: {
+				profile_image: {
+					select: {
+						image_url: true
+					}
 				}
+			}
+		})
 
-				return res.json({success: true, user});
+		if (!user) {
+			return res.status(404).json({success: false, message: "User not found"})
+		}
+
+		return res.json({success: true, user});
+	} catch(e) {
+		return res.status(500).json({success: false, message: "Failed to get user profile", error: e})
+	}
 }
 
+// ERROR: This function is useless until i fix the user profile upload process..
 async function updateUserProfile(req, res) {
-				const user = await prisma.user.findUnique({
-								where: {
-												user_id: Number(req.params.userId)
-								},
-								include: {
-												profile_image: true
-								}
-				})
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				user_id: Number(req.params.userId)
+			},
+			include: {
+				profile_image: true
+			}
+		})
 
-				if (req.user.userid != user.user_id) {
-								return res.status(501).json({success: false, message: "Unauthorized"})
-				}
+		if (req.user.userid != user.user_id) {
+			return res.status(501).json({success: false, message: "Unauthorized"})
+		}
 
-				if (!user) {
-								return res.status(404).json({success: false, message: "User not found"})
-				}
+		if (!user) {
+			return res.status(404).json({success: false, message: "User not found"})
+		}
 
-				const file = req.file;
+		const file = req.file;
 
-				const profile = await uploadProfileImage(file, user);
-				if (!profile) {
-								return res.status(501).json({success: false, message: "Error at the db"})
-				}
+		if (!file) {
+			throw new Error("Profile file not found")
+		}
 
-				return res.json({success: true, profile})
+		const profile = await uploadProfileImage(file, user);
+		if (!profile) {
+			return res.status(501).json({success: false, message: "Error uploading profile image to supabase"})
+		}
+
+		return res.json({success: true, profile})
+	} catch(e) {
+		return res.status(500).json({ success: false, message: "Failed to update user profile", error: e})
+	}
 }
 
 module.exports = {
-				registerUser,
-				loginUser,
-				getUsers,
-				authUser,
-				getUserProfile,
-				updateUserProfile
+	registerUser,
+	loginUser,
+	getUsers,
+	authUser,
+	getUserProfile,
+	updateUserProfile
 }
