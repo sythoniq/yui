e19d92a662579faec922cc07f36d6c137255d119
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-07-17 05:42:26 +0300

    fix: apparently jwt decode doesnt actually verify the users token for auth

--- backend/src/libs/auth.js
+++ backend/src/libs/auth.js
@@ -3,18 +3,21 @@ const jwt = require("jsonwebtoken")
 
 module.exports = (async (req, res, next) => {
 				try {
-								const authHeader = req.headers["authorization"];
-								const token = authHeader.split(" ")[1]
+								const token = req.headers["authorization"].split(" ")[1];
 
 								if (!token) {
 												req.user = null;
 												return next()
 								}
+								jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+												if (error) {
+																req.user = null;
+																return next()
+												}
 
-								const result = jwt.decode(token, process.env.JWT_SECRET);
-								req.user = result;
-
-								return next()
+												req.user = decoded
+												return next()
+								})
 				} catch (e) {
 								req.user = null;
 								return next()
